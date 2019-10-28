df4edc766b202c35b6ca6127ceb6ac2dccc27b9b
Rafael Bohorquez Gomez <rafaelgomezbohorquez2000@gmail.com>
2019-10-28 10:03:32 +0100

    first commit

---
+++ registrar.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+   <p>   <?php echo 'El nombre es ' . $_POST [ 'nombre' ];  ?></p>
+   <p>   <?php echo 'El apellido es ' . $_POST [ 'apellidos' ];  ?></p>
+   <p>   <?php echo 'La edad es ' . $_POST [ 'edad' ];  ?></p>
+   <p>   <?php echo 'La dirección es ' . $_POST [ 'direccion' ];  ?></p>
+</body>
+</html>
